70b283699c965a2f15bc2c58cd86e4cb98dba13b
Thomas J. Re <tomjre@USBs-MacBook-Pro.local>
2016-01-14 13:49:08 +0100

    fixed test (including renaming)- updated pom.html

--- src/test/java/COPDTests.java
+++ src/test/java/COPDTests.java
@@ -1,14 +1,10 @@
 /**
  * 
  */
-package tipl.tests;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
-import tipl.scripts.UFEM;
-import tipl.util.TIPLGlobal;
 
 import org.junit.BeforeClass;
 
@@ -16,27 +12,34 @@ import org.junit.BeforeClass;
  * @author mader
  *
  */
-public class UFEMtests {
+public class COPDTests {
 	@BeforeClass
 	public static void setup() {
-		TIPLGlobal.setDebug(TIPLGlobal.DEBUG_BASIC);
 	}
 	/**
 	 * Test method for {@link tipl.scripts.UFEM#makePoros(tipl.formats.TImg)}.
 	 */
 	@Test
-	public void testMakePoros() {
+	public void testLoadImage() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	/**
+	 * Test method for {@link tipl.scripts.UFEM#makePoros(tipl.formats.TImg)}.
+	 */
+	@Test
+	public void testSegmentLung() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+		/**
+	 * Test method for {@link tipl.scripts.UFEM#makePoros(tipl.formats.TImg)}.
+	 */
+	@Test
+	public void testCalculateScore() {
 		fail("Not yet implemented"); // TODO
 	}
 
-	/**
-	 * Test method for {@link tipl.scripts.UFEM#UFEM(tipl.util.ArgumentParser)}.
-	 */
-	@Test
-	public void testUFEM() {
-		UFEM.main(new String[] {"-ufilt=/Users/mader/Dropbox/tipl/test/simplefoam/distmap.tif","-thresh=15","-resample"});
-		fail("Not yet implemented"); // TODO
-	}
 
 	/**
 	 * Test method for {@link tipl.scripts.UFEM#makePreview(java.lang.String, tipl.formats.TImg)}.
